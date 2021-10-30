01d4a6af14c2506802ae10984916a23295cbd499
Victorcm34 <victorcm34@gmail.com>
2021-10-30 10:25:46 +0200

    start bot

--- Services/TelegramService.cs
+++ Services/TelegramService.cs
@@ -1,7 +1,22 @@
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
 namespace MukiSearchBot.Services
 {
     public class TelegramService
     {
+        private readonly IConfiguration _configuration;
+        private ITelegramBotClient _bot;
+        public TelegramService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _bot = new TelegramBotClient(_configuration["TelegramBotId"]);
+        }
         
+        public void StartBot()
+        {
+            _bot.StartReceiving(null, new CancellationToken());
+        }
     }
 }
